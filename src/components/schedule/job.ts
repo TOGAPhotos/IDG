8e0699919efc00636a5d61e941ce62ccbcfe2cce
794td <davidyan003@gmail.com>
2023-11-06 14:59:06 +0800

    update queue

--- src/components/schedule/job.ts
+++ src/components/schedule/job.ts
@@ -1,19 +1,21 @@
 import nodeSchedule from "node-schedule";
 import {TimerRule_3M,TimerRule_5M,TimeRule_1H} from './rule.js';
 import {GetRandomPhoto,GetStatisticalData,GetPhotoList,} from '../../handler/website/info.js'
+import { GetExcludeList } from "../../handler/queue/get.js";
 import { HourLimit } from "../email/send.js";
 
 export async function StartTimer(){
     await Promise.allSettled([
     GetRandomPhoto(),
     GetStatisticalData(),
+    GetExcludeList(),
     GetPhotoList(),
     ])
     // nodeSchedule.scheduleJob(TimerRule_5M,updateQueueStatus);
     nodeSchedule.scheduleJob(TimerRule_5M,GetPhotoList);
     nodeSchedule.scheduleJob(TimerRule_3M,GetStatisticalData);
     // nodeSchedule.scheduleJob(TimerRule_5M,GetUploadQueueCursor);
-    // nodeSchedule.scheduleJob(TimerRule_5M,GetExcludeList);
+    nodeSchedule.scheduleJob(TimerRule_5M,GetExcludeList);
     nodeSchedule.scheduleJob(TimerRule_5M,GetRandomPhoto);
     nodeSchedule.scheduleJob(TimeRule_1H,HourLimit.reset);
 }
---
+++ src/handler/queue/get.ts
@@ -0,0 +1,55 @@
+import prisma from "./prisma.js";
+import { Request,Response } from "express"
+
+let cursor = -1;
+let excludeList = [];
+
+async function GetUploadQueueCursor() {
+    const dbRes = await  prisma.$queryRawUnsafe(`
+        SELECT
+            id
+        FROM upload_queue
+        WHERE
+            is_delete = 0
+          AND
+            screener_2 IS null
+        ORDER BY id
+        LIMIT 1     
+    `)
+    cursor = dbRes[0].id
+    return dbRes[0].id
+}
+
+export async function GetExcludeList(){
+    cursor = await GetUploadQueueCursor()
+    const dbRes:any[] = await prisma.$queryRawUnsafe(`SELECT id FROM upload_queue WHERE id > ${cursor} AND (is_delete = 1 OR screener_2 IS NOT null)`)
+    
+    excludeList = dbRes.map(obj => { return obj.id } )
+}
+
+export async function GetUploadQueue(req:Request, res:Response) {
+    const photoQueue = await prisma.$queryRawUnsafe(
+        `SELECT
+                a.id AS queue_id,                
+                queue,
+                photo_id AS id,
+                photo_url,
+                airtype,
+                reg,
+                airline,
+                icao,
+                iata,
+                cn_name
+         FROM (SELECT queue, photo_id,id
+               FROM upload_queue
+               WHERE user_id = ${req.token.id} AND is_delete = 0 AND screener_2 IS NULL) AS a,
+              photo b,
+              airport c
+         WHERE a.photo_id = b.id
+           AND b.airport_info = c.id`
+    )
+    if(cursor === -1){
+        await GetExcludeList()
+    }
+    return res.json({message: '查询成功',cursor, photoQueue,excludeList});
+}
