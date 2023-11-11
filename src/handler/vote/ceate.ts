cfc08d8f590d5d060c63c941623400c8062ad074
794TD <davidyan003@gmail.com>
2023-11-11 20:58:04 +0800

    统一使用预先定义的HTTP状态码

--- src/handler/vote/ceate.ts
+++ src/handler/vote/ceate.ts
@@ -4,20 +4,20 @@ import CalculateVote from "./calculate.js";
 export async function CreateVote(req:Request,res:Response) {
     const userInfo = await prisma.user.findUnique({where: {id: req.token.id}});
     if (userInfo.status !== 0) {
-        return res.status(401).json({message: "用户已暂停"});
+        return res.status(HTTP_STATUS.FORBIDDEN).json({message: "用户已暂停"});
     }
 
     if (userInfo.is_deleted) {
-        return res.status(401).json({message: "用户已注销"});
+        return res.status(HTTP_STATUS.FORBIDDEN).json({message: "用户已注销"});
     }
     
-    if (userInfo.total_photo < 5) {
-        return res.status(401).json({message: "权限不足"});
+    if (userInfo.total_photo < 20 || userInfo.role <= 1) {
+        return res.status(HTTP_STATUS.FORBIDDEN).json({message: "您暂时不能发起投票"});
     }
 
     const photoId = Number(req.body.photo_id)
     if(isNaN(photoId)){
-        return res.status(400).json({message: "参数错误"});
+        return res.status(HTTP_STATUS.BAD_REQUEST).json({message: "参数错误"});
     }
 
     const photo = await prisma.photo.findUnique({
@@ -27,11 +27,11 @@ export async function CreateVote(req:Request,res:Response) {
     });
 
     if(photo.is_delete){
-        return res.status(404).json({message: "图片不存在"});
+        return res.status(HTTP_STATUS.NOT_FOUND).json({message: "图片不存在"});
     }
 
     if (photo.vote !== null) {
-        return res.status(400).json({message: "该图片已经有投票了"});
+        return res.status(HTTP_STATUS.BAD_REQUEST).json({message: "该图片已经有投票了"});
     }
 
     let tally = CalculateVote(userInfo.total_photo);
