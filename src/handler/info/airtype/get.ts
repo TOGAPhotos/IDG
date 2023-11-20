67abc70427729bec338341ac157476b736f9f3ca
794TD <davidyan003@gmail.com>
2023-11-20 14:48:02 +0800

    BUG FIX:修改SQL拼接逻辑，去除字符串中的%

--- src/handler/info/airtype/get.ts
+++ src/handler/info/airtype/get.ts
@@ -1,10 +1,23 @@
 import prisma from "../prisma.js";
 import { Request,Response } from "express"
+import { Logger } from "../../../components/loger.js";
+import {ConvertSqlValue} from "../../../components/sql.js";
+
+async function SearchAirType(keyWord:string) {
+    let result = null;
+    try{
+        keyWord = ConvertSqlValue(keyWord);
+        result = await prisma.$queryRawUnsafe(`SELECT id,type,manufacturer,sub_type FROM airtype WHERE is_delete = 0 AND sub_type LIKE '%${keyWord}%' ORDER BY sub_type`);
+    }catch(e){
+        Logger.error(`${e.message}\n${keyWord} 搜索失败`);
+    }
+    return result;
+}
 
 export async function GetAirTypeList(req:Request, res:Response) {
-    let dbResult;
+    let dbResult: unknown;
     if (req.query?.search) {
-        dbResult = await prisma.$queryRawUnsafe(`SELECT id,type,manufacturer,sub_type FROM airtype WHERE is_delete = 0 AND sub_type LIKE '%${req.query.search}%' ORDER BY sub_type`);
+        dbResult = await SearchAirType(<string>req.query.search);
     }else{
         dbResult = await prisma.$queryRawUnsafe('SELECT id,type,manufacturer,sub_type FROM airtype WHERE is_delete = 0 ORDER BY sub_type');
     }
