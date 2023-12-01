c02d47ec2f8f63096e0952320451bae2e6718ebc
794TD <davidyan003@gmail.com>
2023-12-01 15:31:35 +0800

    BUG FIX UPDATE

--- src/components/schedule/job.ts
+++ src/components/schedule/job.ts
@@ -1,10 +1,11 @@
 import nodeSchedule from "node-schedule";
-import {TimerRule_3M,TimerRule_5M,TimeRule_1H} from './rule.js';
-import {GetRandomPhoto,GetStatisticalData,GetPhotoList,} from '../../handler/website/info.js'
+import {TimerRule_10M, TimerRule_3M, TimerRule_5M, TimeRule_1H} from './rule.js';
+import {GetRandomPhoto, GetStatisticalData, GetPhotoList, GetVoteTopList,} from '../../handler/website/info.js'
 import { GetExcludeList } from "../../handler/queue/get.js";
 import { HourLimit } from "../email/send.js";
 import { RenewNotamCache } from "../../handler/notam/cache.js";
-import {updateQueueStatus} from "../../handler/queue/update.js";
+import {UpdateQueueStatus} from "../../handler/queue/update.js";
+import {GetVote} from "../../handler/vote/get.js";
 
 export async function StartTimer(){
     await Promise.allSettled([
@@ -13,12 +14,16 @@ export async function StartTimer(){
     GetExcludeList(),
     GetPhotoList(),
     RenewNotamCache(),
-    ])
-    nodeSchedule.scheduleJob(TimerRule_3M,updateQueueStatus);
+    GetVoteTopList(),
+    ]);
+
+    nodeSchedule.scheduleJob(TimerRule_10M,GetVoteTopList);
+    nodeSchedule.scheduleJob(TimerRule_3M,UpdateQueueStatus);
     nodeSchedule.scheduleJob(TimerRule_5M,GetPhotoList);
     nodeSchedule.scheduleJob(TimerRule_3M,GetStatisticalData);
     nodeSchedule.scheduleJob(TimeRule_1H,RenewNotamCache)
     nodeSchedule.scheduleJob(TimerRule_5M,GetExcludeList);
     nodeSchedule.scheduleJob(TimerRule_5M,GetRandomPhoto);
     nodeSchedule.scheduleJob(TimeRule_1H,HourLimit.reset);
+
 }
