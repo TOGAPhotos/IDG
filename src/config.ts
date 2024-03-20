60bee60fb6197ddb4de9b0a1eae52dfa508f5d79
794TD <davidyan003@gmail.com>
2024-03-20 11:56:09 +0800

    修改配置获取方式

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 import 'dotenv/config'
+
+export const HTTP_PORT = Number(process.env.HTTP_PORT) || 3000;
+
 export const TOKEN_EXPIRE_TIME =  60 * 60 * 24 * 30;
 
 export const photoBaseFolder = '/opt/1panel/apps/openresty/openresty/www/sites/photo.tp.794td.cn/index'
