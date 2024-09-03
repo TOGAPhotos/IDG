ed279c143bbfe599ffcc6f4d697479a9aa81956c
794TD <davidyan003@gmail.com>
2024-09-03 17:20:19 +0800

    chore: Update server import in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 
 import bell from "./components/bell.js";
-import StartHTTPServer from "./app.js";
+import StartHTTPServer from "./server.js";
 
 import 'dotenv/config'
 import ErrorHandler from "./components/errorHandler.js";
 import {startConsoleStr} from "./config.js";
 import RegisterService from "./components/registerService/index.js";
-import { SHARE_ENV } from "worker_threads";
 
 console.log(startConsoleStr);
 
@@ -16,8 +15,8 @@ process.on('unhandledRejection', ErrorHandler.asyncError);
 process.on('exit', code => bell('TOGAPhotos API离线',"退出代码"+code))
 
 
-
 // 启动HTTP服务器
 StartHTTPServer();
+
 // 启动消息队列消费者
 const mailService = new RegisterService('mail', './dist/service/mail/index.js');
