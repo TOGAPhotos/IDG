c14cd0fb0892b5dc157a7668197a3cb253c50bdd
794TD <davidyan003@gmail.com>
2024-09-03 17:20:29 +0800

    chore: Update bell function to include error message when sending notification fails

--- src/components/bell.ts
+++ src/components/bell.ts
@@ -2,16 +2,14 @@ import {BELL_URL} from "../config.js";
 import Log from './loger.js';
 
 export default async function bell(title:string,message=""){
-    Log.info(`Bell ${title}\n${message}`);
-
     try{
         await fetch(BELL_URL,{
             method:'POST',
             headers:{'Content-type':'application/json'},
             body:JSON.stringify({title:title, desp:message})
         });
+        Log.info(`Bell ${title}:${message}`);
     }catch(e){
-        Log.error(`Bell Error ${e.message}`)
-        console.error(`Bell Error:${e.message}`)
+        Log.error(`Bell Error ${e.message} unsend message:${JSON.stringify({title:title, desp:message})}`)
     }
 }
