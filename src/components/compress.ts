037dc6c5c2028556489d8c6aeea851902f8f7521
794TD <davidyan003@gmail.com>
2024-09-10 00:27:56 +0800

    Rebuild this compress function

--- src/components/compress.ts
+++ src/components/compress.ts
@@ -1,35 +1,23 @@
 import gm from 'gm';
-import {photoBaseFolder} from '../config.js';
-import Log from "./loger.js";
 
 export async function GetMinImage(url: string) {
 
-    let w: number, h: number
-    let minH: number, startY: number;
+    const outputImg  = url.split(".")[0] + "_min." + url.split(".")[1]
 
-    await new Promise<void>((resolve, rejects) => {
-        gm(photoBaseFolder + url)
-            .size((err, size) => {
-                if (err) {
-                    Log.error("Compress Error " + err.message)
-                    rejects()
-                }
-                h = size.height
-                w = size.width
-                resolve()
-            })
+    const {width,height} = await new Promise<gm.Dimensions>(
+        (resolve, reject) => gm(url).size((err, size) => err ? reject(err) : resolve(size))
+    );
+    const minH = Math.round((width * 9) / 16);
+    const startY = Math.round((height - 20 - minH) / 2);
+
+    await new Promise((resolve, reject)=>{
+        gm(url)
+        .crop(width, minH, 0, startY)
+        .resize(480)
+        .write(
+            `${outputImg}`,
+            (err) => err ? reject(err) :resolve
+        )
     })
 
-    minH = Math.round((w * 9) / 16);
-    startY = Math.round((h - 20 - minH) / 2);
-    gm(photoBaseFolder + url)
-        .crop(w, minH, 0, startY)
-        .write(
-            `${photoBaseFolder}/min${url}`,
-            err => {
-                if (err) {
-                    Log.error("Compress Error " + err.message)
-                }
-            }
-        )
 }
