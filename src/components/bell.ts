eb0be9d9552a51bfbe2d81a0d25ebe098bf96f86
794TD <davidyan003@gmail.com>
2025-02-07 01:10:18 +0800

    Refactor: Update bell function to handle production and development environments; improve logging for message sending

--- src/components/bell.ts
+++ src/components/bell.ts
@@ -1,15 +1,46 @@
-import {BELL_URL} from "../config.js";
+import {BELL_URL, PRODUCTION_ENV} from "../config.js";
 import Log from './loger.js';
 
-export default async function bell(title:string,message=""){
-    try{
-        await fetch(BELL_URL,{
-            method:'POST',
-            headers:{'Content-type':'application/json'},
-            body:JSON.stringify({title:title, desp:message})
-        });
-        Log.info(`Bell ${title}:${message}`);
-    }catch(e){
-        Log.error(`Bell Error ${e.message} unsend message:${JSON.stringify({title:title, desp:message})}`)
+// export default async function bell(title:string,message=""){
+//     try{
+//         await fetch(BELL_URL,{
+//             method:'POST',
+//             headers:{'Content-type':'application/json'},
+//             body:JSON.stringify({title:title, desp:message})
+//         });
+//         Log.info(`Bell ${title}:${message}`);
+//     }catch(e){
+//         Log.error(`Bell Error ${e.message} unsend message:${JSON.stringify({title:title, desp:message})}`)
+//     }
+// }
+
+
+const bell = function(){
+    return async function(title:string,message=""){
+        if(PRODUCTION_ENV){
+            try{
+                await fetch(BELL_URL,{
+                    method:'POST',
+                    headers:{'Content-type':'application/json'},
+                    body:JSON.stringify({title:title, desp:message})
+                });
+                Log.info(`Bell ${title}:${message}`);
+            }catch(e){
+                Log.error(`Bell Error ${e.message} unsend message:${JSON.stringify({title:title, desp:message})}`)
+            }
+        }else{
+            title = `[DEV_ENV]${title}`;
+            try{
+                await fetch(BELL_URL,{
+                    method:'POST',
+                    headers:{'Content-type':'application/json'},
+                    body:JSON.stringify({title:title, desp:message})
+                });
+                Log.info(`Bell ${title}:${message}`);
+            }catch(e){
+                Log.error(`Bell Error ${e.message} unsend message:${JSON.stringify({title:title, desp:message})}`)
+            }
+        }
     }
-}
+}()
+export default bell;
