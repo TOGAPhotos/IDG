735e6be6e692d08e4515bc5aadd936f5ff3d8743
794TD <davidyan003@gmail.com>
2025-02-07 01:23:55 +0800

    Refactor: Improve fail response handling to utilize error messages based on status codes

--- src/exntend/response.ts
+++ src/exntend/response.ts
@@ -1,3 +1,5 @@
+import { ERROR_MSG } from "@/types/http_code.js";
+
 export function success(msg:any):void;
 export function success(msg:string,data: any):void
 export function success(...args:any[]):void{
@@ -11,18 +13,19 @@ export function success(...args:any[]):void{
         return this.status(200).json({msg:msg});
     }
 
-    throw new Error('Response Success Function Param Missing');
-
 }
-
+export function fail(statusCode:number):void;
 export function fail(statusCode:number,msg:string):void;
 export function fail(statusCode:number,msg:string,data:any):void;
 export function fail(...args:any[]):void{
-    if (args.length === 3){
-        const [statusCode,msg,data] = args
-        return this.status(statusCode).json({msg:msg,data:data});
-    }else if (args.length === 2){
-        const [statusCode,msg] = args
-        return this.status(statusCode).json({msg:msg});
+    const [statusCode, msg, data] = args;
+    if (args.length === 1) {
+        return this.status(statusCode).json({ msg: ERROR_MSG[statusCode] });
+    }
+    if (args.length === 2) {
+        return this.status(statusCode).json({ msg });
+    }
+    if (args.length === 3) {
+        return this.status(statusCode).json({ msg, data });
     }
 }
