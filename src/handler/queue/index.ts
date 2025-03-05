b73ec5bf82af3af4456ec50b9b7c2dd45c647970
794TD <davidyan003@gmail.com>
2025-03-05 17:28:28 +0800

    更新: 优化上传队列处理逻辑，调整缓存设置和错误处理，增加拒绝队列功能

--- src/handler/queue/index.ts
+++ src/handler/queue/index.ts
@@ -3,7 +3,6 @@ import UploadQueue from "../../dto/uploadQueue.js";
 import {UploadQueueCache} from "../../service/redis/uploadQueueCache.js";
 import Permission from "../../components/auth/permissions.js";
 import User from "@/dto/user.js";
-import {GetMinImage} from "@/components/compress.js";
 import { HTTP_STATUS } from "@/types/http_code.js";
 
 export default class QueueHandler {
@@ -20,9 +19,9 @@ export default class QueueHandler {
         const screenCache = await QueueHandler.uploadQueueCache.get(queueId);
 
         if(screenCache !== null && Number(screenCache) !== req.token.id){
-            return res.status(HTTP_STATUS.BAD_REQUEST).json({message: '其他审图员正在审核中'});
+            return res.fail(HTTP_STATUS.CONFLICT,'其他审图员正在审核中');
         }else{
-            await QueueHandler.uploadQueueCache.update(queueId, req.token.id);
+            await QueueHandler.uploadQueueCache.set(queueId, req.token.id);
         }
         const result = await UploadQueue.getById(queueId)
 
@@ -31,23 +30,23 @@ export default class QueueHandler {
 
     static async getQueueTop(req: Request, res: Response) {
         const cursor = Number(req.query['cursor']) || 0;
-        const userInfo = await User.getById(req.token.id);
+        const screener = await User.getById(req.token.id);
 
         const MAX_TRY = 10;
         
         for(let counter = 0;counter<MAX_TRY;counter++){
-            let result = await UploadQueue.getTop(cursor,userInfo.role);
-            if(result.upload_user_id === userInfo.id){ // 跳过自己上传的图片
+            let result = await UploadQueue.getTop(cursor,screener.role);
+            if(result.upload_user_id === screener.id){ // 跳过自己上传的图片
                 continue;
             }
             const cacheInfo = await QueueHandler.uploadQueueCache.get(result.photo_id);
             if(cacheInfo === null || Number(cacheInfo) === req.token.id){
-                await QueueHandler.uploadQueueCache.set(result.photo_id,userInfo.id);
+                await QueueHandler.uploadQueueCache.set(result.photo_id,screener.id);
                 return res.success("查询成功",{photoId:result.photo_id});
             }
         }
 
-        return res.fail(HTTP_STATUS.SERVER_ERROR,'未找到');
+        return res.fail(HTTP_STATUS.LOOP_DETECTED)
     }
 
     static async beater(req: Request, res: Response) {
@@ -68,7 +67,7 @@ export default class QueueHandler {
     static async processScreenResult(req: Request, res: Response) {
         const queueId = Number(req.params['id']);
         const screenerId = req.token.id
-        const screenerInfo = await User.getById(screenerId);
+        const screener = await User.getById(screenerId);
         let finishScreen = false;
 
         const queuePhoto = await UploadQueue.getById(queueId);
@@ -84,18 +83,17 @@ export default class QueueHandler {
             screener_message: string,
             screener_2?: number,
             need_screener_2?: boolean
+        } = {
+            result: req.body['result'],
+            reason: req.body['reason'],
+            screener_message: req.body["screener_message"],
         }
 
-        if (queuePhoto['screener_1'] === null) {
-            screenData = {
-                screener_1: req.token.id,
-                result: req.body['result'],
-                reason: req.body['reason'],
-                screener_message: req.body["screener_message"],
-                need_screener_2: req.body['need_screener_2'] === 1
-            }
+        if (queuePhoto.screener_1 === null) {
+            screenData.screener_1 = req.token.id;
+            screenData.need_screener_2 = req.body['need_screener_2'] === 1;
             if (
-                Permission.isSeniorScreener(screenerInfo.role) &&
+                Permission.isSeniorScreener(screener.role) &&
                 !req.body["need_screener_2"]
             ) {
                 screenData.screener_2 = req.token.id;
@@ -105,14 +103,9 @@ export default class QueueHandler {
         } else if (
             queuePhoto.screener_1 !== null &&
             queuePhoto.screener_2 === null &&
-            Permission.isSeniorScreener(screenerInfo.role)
+            Permission.isSeniorScreener(screener.role)
         ) {
-            screenData = {
-                screener_2: req.token.id,
-                result: req.body['result'],
-                reason: req.body['reason'],
-                screener_message: req.body["screener_message"],
-            }
+            screenData.screener_2 = req.token.id;
             finishScreen = true;
         }else{
             return res.fail(HTTP_STATUS.BAD_REQUEST,'错误请求');
@@ -122,6 +115,31 @@ export default class QueueHandler {
             await UploadQueue.update(queueId, {status: req.body['result']});
             await User.updatePassingRate(queuePhoto.upload_user_id);
         }
+        return res.success('success', {id:queueId, ...screenData});
+    }
+
+    static async rejcetQueue(req: Request, res: Response) {
+        const userId = Number(req.params['id']);
+        const uploadQueue = await UploadQueue.getByUserId(userId);
+        const updateTasks = uploadQueue.map(async (photo)=>{
+            const screening = await QueueHandler.uploadQueueCache.get(photo.id)
+            if(
+                screening !== null && 
+                Number(screening) !== req.token.id
+            ){
+                return {id:photo, result:false}
+            }
+            await UploadQueue.update(photo.id, {
+                screener_1: req.token.id,
+                screener_2: req.token.id,
+                status: 'REJECT',
+                result: 'REJECT',
+                reason: "图片质量不佳",
+            });
+            return {id:photo.id, result:true}
+        })
+        const results = await Promise.allSettled(updateTasks);
+        return res.success('success',results);
     }
 
     static async getQueue(req: Request, res: Response) {
@@ -135,8 +153,6 @@ export default class QueueHandler {
                 return res.success('查询成功', dbRes);
         }
     }
-
-    
 
     static async getScreenedPhoto(req: Request, res: Response) {
         const result = await UploadQueue.recentScreenPhoto();
