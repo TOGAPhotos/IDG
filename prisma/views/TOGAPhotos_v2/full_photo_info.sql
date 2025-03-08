d03ded3a9462cb6abad6191c5e8b83830bb2f22b
794TD <davidyan003@gmail.com>
2025-03-08 23:06:16 +0800

    更新: 重构TOGAPhotos视图，迁移到TOGAPhotos_v2，删除旧视图并添加新查询

--- prisma/views/TOGAPhotos_v2/full_photo_info.sql
+++ prisma/views/TOGAPhotos_v2/full_photo_info.sql
@@ -10,7 +10,7 @@ SELECT
   `p`.`reason` AS `reason`,
   `p`.`screener_message` AS `screener_message`,
   `p`.`upload_user_id` AS `upload_user_id`,
-  `TOGAPhotos`.`user`.`username` AS `username`,
+  `TOGAPhotos_v2`.`user`.`username` AS `username`,
   `p`.`ac_type` AS `ac_type`,
   `p`.`ac_reg` AS `ac_reg`,
   `p`.`ac_msn` AS `ac_msn`,
@@ -25,22 +25,28 @@ SELECT
   `a`.`airport_en` AS `airport_en`,
   `a`.`icao_code` AS `airport_icao_code`,
   `a`.`iata_code` AS `airport_iata_code`,
-  `TOGAPhotos`.`airline`.`airline_cn` AS `airline_cn`,
-  `TOGAPhotos`.`airline`.`airline_en` AS `airline_en`,
-  `TOGAPhotos`.`airline`.`icao_code` AS `airline_icao_code`,
-  `TOGAPhotos`.`airline`.`iata_code` AS `airline_iata_code`
+  `TOGAPhotos_v2`.`airline`.`airline_cn` AS `airline_cn`,
+  `TOGAPhotos_v2`.`airline`.`airline_en` AS `airline_en`,
+  `TOGAPhotos_v2`.`airline`.`icao_code` AS `airline_icao_code`,
+  `TOGAPhotos_v2`.`airline`.`iata_code` AS `airline_iata_code`
 FROM
   (
     (
       (
-        `TOGAPhotos`.`photo` `p`
-        LEFT JOIN `TOGAPhotos`.`airport` `a` ON((`p`.`airport_id` = `a`.`id`))
+        `TOGAPhotos_v2`.`photo` `p`
+        LEFT JOIN `TOGAPhotos_v2`.`airport` `a` ON((`p`.`airport_id` = `a`.`id`))
       )
-      LEFT JOIN `TOGAPhotos`.`user` ON(
-        (`p`.`upload_user_id` = `TOGAPhotos`.`user`.`id`)
+      LEFT JOIN `TOGAPhotos_v2`.`user` ON(
+        (
+          `p`.`upload_user_id` = `TOGAPhotos_v2`.`user`.`id`
+        )
       )
     )
-    LEFT JOIN `TOGAPhotos`.`airline` ON((`p`.`airline_id` = `TOGAPhotos`.`airline`.`id`))
+    LEFT JOIN `TOGAPhotos_v2`.`airline` ON(
+      (
+        `p`.`airline_id` = `TOGAPhotos_v2`.`airline`.`id`
+      )
+    )
   )
 WHERE
   (`p`.`is_delete` = 0)
