713003a04db36586d6c971022deb684a54157394
794TD <davidyan003@gmail.com>
2025-03-25 20:28:01 +0800

    更新: 优化secureSqlString装饰器，只检查字符类型

--- src/components/decorators/secureSqlString.ts
+++ src/components/decorators/secureSqlString.ts
@@ -1,22 +1,15 @@
 export function secureSqlString(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 
     const originalMethod = descriptor.value;
-
-    descriptor.value = function(...args: any[]) {
-
+    descriptor.value = function (...args: any[]) {
         const sanitizedArgs = args.map(arg => {
-            // Convert all arguments to strings
-            const strArg = arg.toString();
-            // Sanitize the string argument
-            return strArg.replace(/--/g, "")
+            if (typeof arg !== "string") return arg;
+            return arg.replace(/--/g, "")
                 .replace(/'/g, "''")
                 .replace(/%/g, "");
         });
-
-        // Call the original method with sanitized arguments
         return originalMethod.apply(this, sanitizedArgs);
     };
 
-    // Return modified descriptor
     return descriptor;
 }
