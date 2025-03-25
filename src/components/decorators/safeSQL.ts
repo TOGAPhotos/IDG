594b41f46e354c5b03a9f20821145341f54c439a
794TD <davidyan003@gmail.com>
2025-03-25 20:50:35 +0800

    新增: 重命名secureSqlString为safeSQL装饰器，增加了对Object的检查

---
+++ src/components/decorators/safeSQL.ts
@@ -0,0 +1,36 @@
+const strCheck = (str: string) => {
+    return str.replace(/--/g, "")
+        .replace(/'/g, "''")
+        .replace(/%/g, "");
+}
+
+const objCheck = (obj: any) => {
+    for (let key in obj) {
+        if (obj[key] && typeof obj[key] === "string") {
+            obj[key] = strCheck(obj[key]);
+        }
+        if (obj[key] && typeof obj[key] === "object") {
+            obj[key] = objCheck(obj[key]);
+        }
+    }
+    return obj;
+}
+
+export function safeSQL(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        const sanitizedArgs = args.map(arg => {
+            if (typeof arg === "string") {
+                return strCheck(arg);
+            }
+            if (typeof arg === "object") {
+                return objCheck(arg);
+            }
+            return arg;
+        });
+        return originalMethod.apply(this, sanitizedArgs);
+    };
+
+    return descriptor;
+}
