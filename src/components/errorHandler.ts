889dd2d79dd70ea7457fc203c80b501619ef2fe9
794TD <davidyan003@gmail.com>
2025-07-27 19:41:37 +0800

    refactor: improve formatting and structure of error handling classes

--- src/components/errorHandler.ts
+++ src/components/errorHandler.ts
@@ -1,12 +1,11 @@
 import Log from "./loger.js";
 
-export default class ErrorHandler{
-    static syncError(err:Error){
-        Log.error(err.message+'\n'+err.stack);
-    }
+export default class ErrorHandler {
+  static syncError(err: Error) {
+    Log.error(err.message + "\n" + err.stack);
+  }
 
-    static asyncError(reason:Error){
-        Log.error(reason.message+'\n'+reason.stack);
-    }
-
+  static asyncError(reason: Error) {
+    Log.error(reason.message + "\n" + reason.stack);
+  }
 }
