27e4f01db5a0a54e5d1c15caa69b2a8981f21893
794TD <davidyan003@gmail.com>
2025-09-29 12:21:29 -0400

    feat: enhance logging functionality with server-sent events and debug mode

--- src/components/loger.ts
+++ src/components/loger.ts
@@ -4,6 +4,7 @@ import chalk from "chalk";
 import { type Request, type Response, type NextFunction } from "express";
 import Time from "./time.js";
 import { PRODUCTION_ENV } from "../config.js";
+import { publishLogEvent } from "./logStream.js";
 
 const customFormat = format.combine(
   format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
@@ -40,56 +41,53 @@ const Logger = createLogger({
 });
 
 export default class Log {
-  static success(message: string) {
-    console.log(chalk.bgGreen.bold(Time.getUTCTime() + ": " + message));
-    Logger.info(message);
+  private static DEBUG_MODE = !PRODUCTION_ENV;
+  static setDebugMode(mode: boolean) {
+    Log.DEBUG_MODE = mode;
+    Log.warn("Debug mode set to " + mode);
   }
-
-  static debug = (() => {
-    if (PRODUCTION_ENV) {
-      return () => {};
-    } else {
-      return (message: string) => {
-        console.debug(Time.getUTCTime() + ": " + message);
-        Logger.debug(message);
-      };
+  static debug = (message:string) => {
+    if (!Log.DEBUG_MODE) {
+      return;
     }
-  })();
+      console.debug(Time.getUTCTime() + ": " + message);
+      Logger.debug(message);
+      publishLogEvent("debug", message);
+  }
 
   static info(message: string) {
     console.log(Time.getUTCTime() + ": " + message);
     Logger.info(message);
+    publishLogEvent("info", message);
   }
 
   static warn(message: string) {
     console.warn(chalk.bgYellow.bold(Time.getUTCTime() + ": " + message));
     Logger.warn(message);
+    publishLogEvent("warn", message);
   }
 
   static error(message: string) {
     console.error(chalk.bgRed.bold(Time.getUTCTime() + ": " + message));
     Logger.error(message);
+    publishLogEvent("error", message);
   }
 
-  static accessLogMW() {
-    if (PRODUCTION_ENV) {
-      return function (req: Request, res: Response, next: NextFunction) {
-        req.userIp = (req.headers["x-real-ip"] as string) || req.ip;
-        req.tId = (req.headers["t_id"] as string) || "NO_TRACE_ID";
+  static accessLogMW(req: Request, res: Response, next: NextFunction) {
+    req.userIp = (req.headers["x-real-ip"] as string) || req.ip;
+    req.tId = (req.headers["t_id"] as string) || "NO_TRACE_ID";
+    if (!Log.DEBUG_MODE) {
         Logger.info(
           `${req.userIp} ${req.method} ${req.url} userId:${req.token?.id || "NOT LOGIN"} trace_id:${req.tId} ${JSON.stringify(req.body)}`,
         );
-        next();
-      };
     } else {
-      return function (req: Request, res: Response, next: NextFunction) {
-        req.userIp = (req.headers["x-real-ip"] as string) || req.ip;
-        req.tId = (req.headers["t_id"] as string) || "NO_TRACE_ID";
-        Log.debug(
-          `${req.userIp} ${req.method} ${req.url} userId:${req.token?.id || "NOT LOGIN"} trace_id:${req.tId} ${JSON.stringify(req.body)}`,
-        );
-        next();
-      };
+      Logger.info(
+        `${req.userIp} ${req.method} ${req.url} userId:${req.token?.id || "NOT LOGIN"} trace_id:${req.tId} ${JSON.stringify(req.body)}`,
+      );
+      Log.debug(
+        `${req.userIp} ${req.method} ${req.url} userId:${req.token?.id || "NOT LOGIN"} trace_id:${req.tId} ${JSON.stringify(req.body)}`,
+      );
     }
+    next();
   }
 }
