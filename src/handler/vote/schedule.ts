a1848e7373913c31265c59997d2c2acb0108a63d
794TD <davidyan003@gmail.com>
2025-10-11 18:18:28 -0400

    feat: update tallySCVote to improve vote processing and handle pic_type correctly

--- src/handler/vote/schedule.ts
+++ src/handler/vote/schedule.ts
@@ -3,7 +3,7 @@ import Log from "../../components/loger.js";
 const prisma = new PrismaClient();
 
 export async function tallySCVote() {
-  Log.info("SC投票结算");
+  Log.info("Start tally SC vote");
   const SCVotes = await prisma.vote_list.findMany({
     where: {
       status: "IN_PROGRESS",
@@ -13,38 +13,26 @@ export async function tallySCVote() {
     },
   });
   if (SCVotes.length === 0) {
-    return Log.info("没有需要结算的SC投票");
+    return Log.info("No SCVote event need to process");
   }
-  // await prisma.$executeRawUnsafe(`
-  //     UPDATE TOGAPhotos_v2.vote_list
-  //     JOIN TOGAPhotos_v2.photo p on vote_list.photo_id = p.id
-  //     SET vote_list.status = 'END',
-  //       tally = (
-  //           SELECT sum(tally)
-  //           FROM TOGAPhotos_v2.vote_record
-  //           WHERE vote_event = TOGAPhotos_v2.vote_list.id
-  //       ),
-  //       p.pic_type = IF(
-  //               (SELECT sum(tally)
-  //                        FROM TOGAPhotos_v2.vote_record
-  //                        WHERE vote_event = TOGAPhotos_v2.vote_list.id) > 0,
-  //               CONCAT_WS(',',pic_type, 'ScreenerChoice'),
-  //               1)
-  //     WHERE vote_list.status = 'IN PROGRESS'
-  //       AND end < (UNIX_TIMESTAMP() * 1000)
-  //       AND p.id = vote_list.photo_id;
-  // `);
+  await prisma.$executeRawUnsafe(`
+      UPDATE vote_list
+      JOIN photo p on vote_list.photo_id = p.id
+      SET vote_list.status = 'END',
+        tally = (
+            SELECT sum(tally)
+            FROM vote_record
+            WHERE vote_event = vote_list.id
+        ),
+        p.pic_type = IF(
+                (SELECT sum(tally)
+                         FROM vote_record
+                         WHERE vote_event = vote_list.id) > 0,
+                CONCAT_WS(',',NULLIF(pic_type,''), 'ScreenerChoice'),
+                pic_type)
+      WHERE vote_list.status = 'IN PROGRESS'
+        AND vote_list.id IN (${SCVotes.map((v) => v.id).join(",")})
+        AND p.id = vote_list.photo_id;
+  `);
   Log.info(`SC vote tally finished updated:${SCVotes.length}`);
-  // 结算完成后，获取SC图片，准备发送邮件
-  // await prisma.$queryRawUnsafe(`
-  //     SELECT p.id,p.upload_user_id,user_email
-  //     FROM TOGAPhotos_v2.photo p
-  //            JOIN TOGAPhotos_v2.user ON upload_user_id = user.id
-  //     WHERE p.id IN (
-  //       SELECT photo_id
-  //       FROM TOGAPhotos_v2.vote_list
-  //       WHERE id IN (${SCVotes.map((v) => v.id).join(",")})
-  //         AND tally > 0
-  //     )
-  // `);
 }
