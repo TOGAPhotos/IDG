7ee32ebf8677e98ab04d64b0550229bb3a8ed5b1
794TD <davidyan003@gmail.com>
2025-11-09 02:05:14 -0500

    feat: integrate event bus for photo deletion and CDN cache refresh

--- src/service/cdn/index.ts
+++ src/service/cdn/index.ts
@@ -1,5 +1,8 @@
 import { cdn } from "tencentcloud-sdk-nodejs-cdn";
 import { PHOTO_COS_CDN_DOMAIN, TENCENTCLOUD_SECRET_ID, TENCENTCLOUD_SECRET_KEY } from "../../config.js";
+import { EventBus } from "../../components/eventBus/indes.js";
+import Log from "../../components/loger.js";
+
 const cdnClient = cdn.v20180606.Client
 
 export const CDNController = new cdnClient({
@@ -12,3 +15,23 @@ export const CDNController = new cdnClient({
 export const getPhotoCDNUrl = (key: string|number,raw=false): string => {
   return `https://${PHOTO_COS_CDN_DOMAIN}/photos/${key}.${raw?'raw':'jpg'}`
 }
+
+const refreshCDNPath = async (path: string[]): Promise<void> => {
+    await CDNController.PurgeUrlsCache({
+      Urls: path
+    },
+      (err, response) => {
+        if (err) Log.error(err);
+        else Log.info(`CDN refresh path success paths:${path.join(",")} requestId:${response.RequestId} taskId:${response.TaskId}`);
+      }
+    );
+}
+
+const eventBus = new EventBus()
+eventBus.subscribe("photo:delete",(msg,data)=>{
+  const { photoId } = data;
+  return refreshCDNPath([
+    getPhotoCDNUrl(photoId,true),
+    getPhotoCDNUrl(photoId,false)
+  ])
+})
