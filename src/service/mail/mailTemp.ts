646c581109155e18a855e1f18e3ae8398755a6b1
794TD <davidyan003@gmail.com>
2025-11-10 12:41:54 -0500

    chore: standardize email subject prefixes to use 'TOGAPhotos' for consistency

--- src/service/mail/mailTemp.ts
+++ src/service/mail/mailTemp.ts
@@ -27,7 +27,7 @@ export default class MailTemp {
     let email: EmailFormat = {
       sender: "TOGAPhotos 服务状态更新 <StatusUpdate@Togaphotos.com>",
       receiver: emailAddr,
-      subject: `[TO/GA Photos] ${serverName} 服务状态更新`,
+      subject: `[TOGAPhotos] ${serverName} 服务状态更新`,
       template: null,
       content: `${serverName}于${Time.getUTCTime()}状态更新为${status}`,
     };
@@ -58,7 +58,7 @@ export default class MailTemp {
     const email: EmailFormat = {
       sender: "TOGAPhotos 数据管理团队 <DataManagementTeam@togaphotos.com>",
       receiver: emailAddr,
-      subject: "[TO/GA Photos] 数据库补充信息审核结果",
+      subject: "[TOGAPhotos] 数据库补充信息审核结果",
       template: `toga_photos_add_info_${result}`,
       content: JSON.stringify({ cn_name: cn_name, iata: iata, icao: icao }),
     };
@@ -85,7 +85,7 @@ export default class MailTemp {
     const email: EmailFormat = {
       sender: "TOGAPhotos 消息机器人<no-reply@togaphotos.com>",
       receiver: emailAddr,
-      subject: "[TO/GA Photos] 你有一条新的私信",
+      subject: "[TOGAPhotos] 你有一条新的私信",
       template: "togaphotos_directmessage_notice",
       content: JSON.stringify({
         sender_name: sender_name,
@@ -121,11 +121,10 @@ export default class MailTemp {
     const email: EmailFormat = {
       sender: "TOGAPhotos 消息机器人<no-reply@togaphotos.com>",
       receiver: emailAddr,
-      subject: "[TO/GA Photos] 照片审核结果通知",
+      subject: "[TOGAPhotos] 图片审核结果通知",
       template: null,
       content: MailTemp.renderHTMLTemplate("screeningResult",{username, results})
     }
-    Log.debug(JSON.stringify(email));
     await MailTemp.emailQueue.send(JSON.stringify(email));
   }
 }
